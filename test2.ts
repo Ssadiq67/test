37eea722eaa1a11635acaf99a901779460b95454
Ssadiq67 <msohailsadiq@gmail.com>
2025-12-02 16:36:39 +0500

    Describe change

---
+++ test2.ts
@@ -0,0 +1,10 @@
+//let message: string = 'Hello World';
+//console.log(message);
+
+
+
+function sayHello(name:string) {
+  console.log('Hello, ' + name);
+}
+
+sayHello('VS Code with typesetting');
